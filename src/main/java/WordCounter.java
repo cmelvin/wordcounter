5cf03d72e873e7be94b57efaab99822996b8e526
cmel888 <cmel888@gmail.com>
2020-04-27 08:02:38 +0100

    first test total number of words added to list

--- src/main/java/WordCounter.java
+++ src/main/java/WordCounter.java
@@ -1,2 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class WordCounter {
+
+    List<String> wordList = new ArrayList<String>();
+
+    public void add(String word) {
+
+            wordList.add(word);
+
+    }
+
 }
--- src/test/java/WordCounterTest.java
+++ src/test/java/WordCounterTest.java
@@ -1,4 +1,7 @@
 import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class WordCounterTest {
 
@@ -7,9 +10,24 @@ public class WordCounterTest {
 
     @Before
     public void setup()  {
-
-
+        wordCounter.add("Sky");
+        wordCounter.add("Earth");
+        wordCounter.add("Flower");
+        wordCounter.add("Flor");
+        wordCounter.add("Blume");
+        wordCounter.add("Vineto");
+        wordCounter.add("Water");
+        wordCounter.add("Earth");
+        wordCounter.add("Fire");
+        wordCounter.add("Animal");
+        wordCounter.add("Human");
+        wordCounter.add("Animal");
+        wordCounter.add("Song");
     }
 
 
+    @Test
+    public void totalCountOfWeCounterList(){
+        assertEquals(13,wordCounter.wordList.size());
+    }
 }
