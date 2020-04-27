6ed50a4fba34e7e6d7306b6de379458c81684b29
cmel888 <cmel888@gmail.com>
2020-04-27 08:18:01 +0100

    Implementation for the Functional Interface TranslateFunction

--- src/main/java/com/translator/Translator.java
+++ src/main/java/com/translator/Translator.java
@@ -2,7 +2,14 @@ package com.translator;
 
 public class Translator {
 
+    TranslatorMethods translatorMethods = new TranslatorMethods();
+
     public String translate(String input) {
-        return "";
+        return translatedWord(translatorMethods::translate, input);
+
+    }
+
+    public String translatedWord(TranslatorFunction tf, String input) {
+        return tf.translate(input);
     }
 }
